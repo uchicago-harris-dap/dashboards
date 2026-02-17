adea9799b5f515bbbba190ff855b04ab8165ddb9
Maggie Shi <m.shi@uchicago.edu>
2026-02-17 10:51:37 -0600

    add simpler multipage examples

---
+++ 3/multipage_simple.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+# This message is always displayed
+st.write("Welcome message")
+
+# define functions for what each page displays
+def my_first_page():
+    st.write("my first page")
+def my_second_page():
+    st.write("my second page")
+
+# define dictionary for what is in the selection box
+page_names_to_funcs = {
+    "First Page": my_first_page,
+    "Second Page": my_second_page
+}
+
+# define the selection box
+demo_name = st.sidebar.selectbox(
+    "Choose a page", 
+    page_names_to_funcs.keys()
+)
+
+# only run a function if the user has selected that page
+if demo_name == "First Page":
+    my_first_page()
+elif demo_name == "Second Page":
+    my_second_page()
